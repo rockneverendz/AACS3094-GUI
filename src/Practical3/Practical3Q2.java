8f4973c8da8b22ecd36c34e21cc7a8033aceefe7
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2018-11-02 12:17:40 +0800

    Practical 3 Q 2 completed

--- src/Practical3/Practical3Q2.java
+++ src/Practical3/Practical3Q2.java
@@ -16,9 +16,11 @@ public class Practical3Q2 extends JFrame {
         jpnComplete.add(jbtButton);
         jpnComplete.setLayout(new FlowLayout(FlowLayout.RIGHT));
 
+        jbtButton.addActionListener(e -> JpnInput.calc());
+
         add(jpnInput, BorderLayout.CENTER);
         add(jpnComplete, BorderLayout.SOUTH);
-
+        
         super.setTitle("Loan Calculator");
         super.setSize(400, 300);
         super.setLocationRelativeTo(null);
@@ -28,11 +30,11 @@ public class Practical3Q2 extends JFrame {
 }
 
 class JpnInput extends JPanel{
-    JTextField annualInterestRate = new JTextField();
-    JTextField numberOfYears = new JTextField();
-    JTextField loanAmount = new JTextField();
-    JTextField monthlyPayment = new JTextField();
-    JTextField totalPayment = new JTextField();
+    private static JTextField annualInterestRate = new JTextField();
+    private static JTextField numberOfYears = new JTextField();
+    private static JTextField loanAmount = new JTextField();
+    private static JTextField monthlyPayment = new JTextField();
+    private static JTextField totalPayment = new JTextField();
 
     JpnInput(){
         setBorder(new TitledBorder("Enter loan amount, interest rate, and years"));
@@ -46,17 +48,20 @@ class JpnInput extends JPanel{
         add(loanAmount);
         add(new JLabel("Monthly Payment"));
         add(monthlyPayment);
+        monthlyPayment.setEditable(false);
         add(new JLabel("Total Payment"));
         add(totalPayment);
+        totalPayment.setEditable(false);
     }
 
-    void JpnInputCalc(){
-        double x = Double.parseDouble(annualInterestRate.toString());
-        int y = Integer.parseInt(numberOfYears.toString());
-        double z = Double.parseDouble(loanAmount.toString());
+    static void calc(){
+        double x2 = Double.parseDouble(annualInterestRate.getText());
+        int y2 = Integer.parseInt(numberOfYears.getText());
+        double z2 = Double.parseDouble(loanAmount.getText());
 
-        Loan loan = new Loan(x, y, z);
+        Loan loan = new Loan(x2, y2, z2);
 
-
+        monthlyPayment.setText(String.format("%.2f",loan.getMonthlyPayment()));
+        totalPayment.setText(String.format("%.2f",loan.getTotalPayment()));
     }
 }
