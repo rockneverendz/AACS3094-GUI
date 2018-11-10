5affd38f3b53ee34e47dc6da01947964ca9a3ce9
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2018-11-10 17:19:09 +0800

    P4Q4

--- src/Practical4/Practical4Q4.java
+++ src/Practical4/Practical4Q4.java
@@ -1,3 +1,5 @@
+import com.sun.jdi.InvalidLineNumberException;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,14 +11,29 @@ public class Practical4Q4 extends JFrame {
     private Practical4Q4() {
         getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-        JPanel JpnPassword = new JpnPassword();
+        JPanel JpnPassword = new JPanel();
+        JPasswordField jpfPassword = new JPasswordField();
         JButton jbtSubmit = new JButton("Submit");
-        JTextPane jtpLog = new JTextPane();
+        JTextArea jtaLog = new JTextArea();
+
+        JpnPassword.setLayout(new GridLayout(1, 2));
+        JpnPassword.add(new JLabel("Enter your password"));
+        JpnPassword.add(jpfPassword);
 
         add(JpnPassword);
         add(jbtSubmit);
-        add(jtpLog);
-        jtpLog.setPreferredSize( new Dimension( 200, 200 ) );
+        add(jtaLog);
+        jtaLog.setPreferredSize(new Dimension(200, 200));
+        //jtaLog.setFont("SANS",Color.RED);
+        jbtSubmit.addActionListener(e -> {
+            try {
+                Password password = new Password(jpfPassword.getPassword());
+                jtaLog.setText("Valid!");
+            } catch (InvalidPasswordException ex) {
+                jtaLog.setText(ex.getMessage());
+
+            }
+        });
 
         //jbtSubmit.addActionListener();
 
@@ -28,14 +45,47 @@ public class Practical4Q4 extends JFrame {
     }
 }
 
-class JpnPassword extends JPanel {
-    private static JPasswordField jtfPassword = new JPasswordField(15);
+class Password {
+    private String password = "";
 
-    JpnPassword() {
-        super.setLayout(new GridLayout(1, 2));
-        add(new JLabel("Enter your password"));
-        add(jtfPassword);
+    Password() {
     }
 
+    Password(char[] password) throws InvalidPasswordException {
+        StringBuilder errorMessage = new StringBuilder();
+        boolean containLetter = false;
+        boolean containDigit = false;
 
+        if (password.length == 0)
+            errorMessage.append("Cannot Null/Empty");
+        if (password.length < 6)
+            errorMessage.append("Cannot less then 7 character\n");
+
+        for (int i = 0; i < password.length; i++)
+            if (Character.isLetter(password[i])) {
+                containLetter = true;
+                i = password.length;
+            }
+
+        for (int i = 0; i < password.length; i++)
+            if (Character.isDigit(password[i])) {
+                containDigit = true;
+                i = password.length;
+            }
+
+        if (!containLetter)
+            errorMessage.append("Your password should have at least 1 letter\n");
+        if (!containDigit)
+            errorMessage.append("Your password should have at least 1 digit\n");
+
+        if (!errorMessage.toString().isEmpty())
+            throw new InvalidPasswordException(errorMessage.toString());
+    }
+}
+
+class InvalidPasswordException extends Exception {
+    public InvalidPasswordException() {}
+    InvalidPasswordException(String message) {
+        super(message);
+    }
 }
